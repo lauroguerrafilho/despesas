840b977b8de63d74f601b7466858e082d5abff44
lauroguerrafilho <62521270+lauroguerrafilho@users.noreply.github.com>
2023-07-02 21:00:11 -0300

    Delete schemas directory

--- schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from schemas.despesas import DespesasSchema, DespesasBuscaSchema, DespesasViewSchema, \
-                            ListagemDespesasSchema, DespesasDelSchema, apresenta_despesas, \
-                            apresenta_despesa
-from schemas.error import ErrorSchema
